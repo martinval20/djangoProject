61dcf3ccfd0cddac7d32a157748223d17f9fc858
Martin Valdiviezo <martinval2000@hotmail.com>
2025-05-18 21:50:17 -0500

    forms

--- myapp/views.py
+++ myapp/views.py
@@ -1,7 +1,8 @@
 
 from django.http import HttpResponse
 from .models import Project, Task
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import CreateNewTask
 # Create your views here.
 def index(request):
     tittle = 'Django Course!!!'
@@ -32,3 +33,12 @@ def tasks(request):
         'tasks': tasks
     })
 
+def create_task(request):
+    if request.method == 'GET':
+        return render(request, 'create_task.html', {
+        'form': CreateNewTask()
+        })
+    else:
+        Task.objects.create(tittle=request.POST['tittle'], description= request.POST['description'], project_id=1)
+        return redirect('/tasks/')
+    
